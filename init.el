9de3a92ad1500e506fcf75d7998756c95b5ff410
12pt <12pt@users.noreply.github.com>
2017-06-06 00:20:55 +0100

    Catch up with alterations made over the last few months.

--- init.el
+++ init.el
@@ -21,12 +21,6 @@
  '(cperl-indent-level 4)
  '(cperl-indent-parens-as-block t)
  '(cperl-tab-always-indent t)
- '(custom-safe-themes
-   (quote
-    ("eb0a314ac9f75a2bf6ed53563b5d28b563eeba938f8433f6d1db781a47da1366" default)))
- '(org-agenda-files
-   (quote
-    ("~/Documents/DSPARP/part1/final-rewrite.org" "/home/user/org/appointments.org" "/home/user/org/coursework.org" "/home/user/org/elfeed.org" "/home/user/org/habits.org" "/home/user/org/notes.org" "/home/user/org/organiser.org" "/home/user/org/personal.org" "/home/user/org/todo.org")))
  '(org-src-lang-modes
    (quote
     (("ocaml" . tuareg)
@@ -45,7 +39,7 @@
      ("bash" . sh))))
  '(package-selected-packages
    (quote
-    (evil-tutor fuel org-indent-mode org-indent pug-mode elfeed-goodies elfeed-org which-key web-mode use-package smart-tabs-mode rainbow-delimiters projectile-speedbar projectile powerline org-evil org-dashboard neotree moe-theme magit lua-mode jsx-mode json-mode js2-refactor google git-gutter-fringe flymake-perlcritic evil-surround evil-org elfeed edbi-sqlite devdocs counsel company-tern clj-refactor all-the-icons))))
+    (indium org-bullets apropospriate-theme airline-themes demo-it tide markdown-mode darcula-theme evil-tutor fuel org-indent-mode org-indent pug-mode elfeed-goodies elfeed-org which-key web-mode use-package smart-tabs-mode rainbow-delimiters projectile-speedbar projectile powerline org-evil org-dashboard neotree moe-theme magit lua-mode jsx-mode json-mode js2-refactor google git-gutter-fringe flymake-perlcritic evil-surround evil-org elfeed edbi-sqlite devdocs counsel company-tern clj-refactor all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
