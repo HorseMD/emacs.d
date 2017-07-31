f70aedd9df5a40d09d4fb1eeb5e8811b9d3f5513
12pt <12pt@users.noreply.github.com>
2017-07-31 15:49:11 +0100

    Refactoring

--- init.el
+++ init.el
@@ -1,8 +1,5 @@
 ;; Assumes that a copy of org-mode is in ~/.emacs.d/org-mode/.
 
-(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
-(add-to-list 'load-path "~/.emacs.d/org-mode/contrib/lisp")
-
 (package-initialize)
 (setq package-enable-at-startup nil)
 
--- install.bat
+++
@@ -1,7 +0,0 @@
-set homedir=%userprofile%
-set cwd=%~dp0
-
-mklink "%homedir%\AppData\Roaming\.emacs.d\daniel.org" "%cwd%daniel.org"
-mklink "%homedir%\AppData\Roaming\.emacs.d\init.el"    "%cwd%init.el"
-
-pause
