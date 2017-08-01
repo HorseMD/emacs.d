368f57cb29223d6d62aede597eb6b43e4a23a5de
12pt <12pt@users.noreply.github.com>
2017-08-01 11:00:35 +0100

    Refactoring, deadheading.

--- init.el
+++ init.el
@@ -6,4 +6,28 @@
 (require 'org)
 (require 'ob-tangle)
 
+;; dont prompt me every time I open symlinks.
+(setq vc-follow-symlinks t)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/daniel.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cperl-array-face ((t nil)) t)
+ '(cperl-close-paren-offset -4)
+ '(cperl-continued-statement-offset 4)
+ '(cperl-indent-level 4)
+ '(cperl-indent-parens-as-block t)
+ '(cperl-tab-always-indent t)
+ '(package-selected-packages
+   (quote
+    (which-key web-mode use-package tide rainbow-delimiters org-ref org-evil org-bullets markdown-mode lua-mode json-mode js2-refactor git-gutter-fringe geiser flymake-perlcritic evil-surround evil-quickscope counsel-projectile company-tern clj-refactor apropospriate-theme all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
